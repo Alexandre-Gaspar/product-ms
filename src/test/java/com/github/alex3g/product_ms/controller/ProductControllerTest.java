c6d45c1cd1500dfe4a2428d4bd144ceb3b3b8a31
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-08-12 11:35:10 +0100

    feat(tests): add test of method getById of the ProductController
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

--- src/test/java/com/github/alex3g/product_ms/controller/ProductControllerTest.java
+++ src/test/java/com/github/alex3g/product_ms/controller/ProductControllerTest.java
@@ -4,7 +4,10 @@ import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.alex3g.product_ms.dto.ProductDTO;
+import com.github.alex3g.product_ms.model.Product;
+import com.github.alex3g.product_ms.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +30,9 @@ public class ProductControllerTest {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private ProductRepository repository;
+
     @BeforeAll
     static void setUp() {
         FixtureFactoryLoader.loadTemplates("com.github.alex3g.product_ms.template");
@@ -34,13 +41,43 @@ public class ProductControllerTest {
     @Test
     void shouldCreateProduct() throws Exception {
         ProductDTO request = Fixture.from(ProductDTO.class).gimme("valid");
-        String productAsString = mapper.writeValueAsString(request);
+        String productDataAsString = mapper.writeValueAsString(request);
 
         mvc.perform(post("/products")
                         .header(AUTHORIZATION, "Bearer foo")
                         .contentType(APPLICATION_JSON)
-                        .content(productAsString))
+                        .content(productDataAsString))
                 .andExpect(status().isCreated());
     }
 
+    @Test
+    void shouldNotCreateProduct() throws Exception {
+        ProductDTO request = new ProductDTO(); // An empty request
+        String productDataAsString = mapper.writeValueAsString(request);
+
+        mvc.perform(post("/products")
+                        .header(AUTHORIZATION, "Bearer foo")
+                        .contentType(APPLICATION_JSON)
+                        .content(productDataAsString))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldGetAllProducts() throws Exception{
+        mvc.perform(get("/products")
+                        .header(AUTHORIZATION, "Bearer foo"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetById() throws Exception {
+        Product productToCreate = Fixture.from(Product.class).gimme("valid");
+        repository.save(productToCreate);
+
+        Long id = repository.findAll().getFirst().getId();
+
+        mvc.perform(get("/products/{id}", id)
+                        .header(AUTHORIZATION, "Bearer foo"))
+                .andExpect(status().isOk());
+    }
 }
