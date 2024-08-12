e46fe6842a2149bc3ea9430419419955b4d2d2bc
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-08-12 11:33:28 +0100

    feat(tests): add a tamplate of product entity
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

--- src/test/java/com/github/alex3g/product_ms/template/ProductTemplateLoader.java
+++ src/test/java/com/github/alex3g/product_ms/template/ProductTemplateLoader.java
@@ -4,6 +4,7 @@ import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.Rule;
 import br.com.six2six.fixturefactory.loader.TemplateLoader;
 import com.github.alex3g.product_ms.dto.ProductDTO;
+import com.github.alex3g.product_ms.model.Product;
 
 public class ProductTemplateLoader implements TemplateLoader {
     @Override
@@ -12,6 +13,13 @@ public class ProductTemplateLoader implements TemplateLoader {
             add("name", random("Monitor", "iPhone 15", "Mac Book Pro"));
             add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
             add("price", random(Double.class, range(0.01, 199087.89)));
+        }});
+        Fixture.of(Product.class).addTemplate("valid", new Rule(){{
+            add("id", random(Long.class, range(1L, 5L)));
+            add("name", random("Monitor", "iPhone 15", "Mac Book Pro", "Computador HP", "AirPods 11s"));
+            add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
+            add("price", random(Double.class, range(0.01, 199087.89)));
+            add("isAvailable", true);
         }});
     }
 }
