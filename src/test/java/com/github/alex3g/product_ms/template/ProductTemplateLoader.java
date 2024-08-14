6e699af8d49ab5f555f1e316a238f1a231c92be6
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-08-14 06:36:55 +0100

    feat(test): add a new template/fixture for testing the updates

--- src/test/java/com/github/alex3g/product_ms/template/ProductTemplateLoader.java
+++ src/test/java/com/github/alex3g/product_ms/template/ProductTemplateLoader.java
@@ -14,6 +14,11 @@ public class ProductTemplateLoader implements TemplateLoader {
             add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
             add("price", random(Double.class, range(0.01, 199087.89)));
         }});
+        Fixture.of(ProductDTO.class).addTemplate("valid-update", new Rule(){{
+            add("name", random("Monitor DELL", "iPhone 15 Pro", "Mac Book Air - M5"));
+            add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
+            add("price", random(Double.class, range(200, 459087.89)));
+        }});
         Fixture.of(Product.class).addTemplate("valid", new Rule(){{
             add("id", random(Long.class, range(1L, 5L)));
             add("name", random("Monitor", "iPhone 15", "Mac Book Pro", "Computador HP", "AirPods 11s"));
