c9e606f0ecea112ec34840e6ca6067fe0eb92068
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-08-18 02:37:04 +0100

    chore: alter datas in the template/fixture-factory
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

--- src/test/java/com/github/alex3g/product_ms/template/ProductTemplateLoader.java
+++ src/test/java/com/github/alex3g/product_ms/template/ProductTemplateLoader.java
@@ -10,18 +10,18 @@ public class ProductTemplateLoader implements TemplateLoader {
     @Override
     public void load() {
         Fixture.of(ProductDTO.class).addTemplate("valid", new Rule(){{
-            add("name", random("Monitor", "iPhone 15", "Mac Book Pro"));
+            add("name", random("Monitor", "Iphone 15", "Macnook pro"));
             add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
             add("price", random(Double.class, range(0.01, 199087.89)));
         }});
         Fixture.of(ProductDTO.class).addTemplate("valid-update", new Rule(){{
-            add("name", random("Monitor DELL", "iPhone 15 Pro", "Mac Book Air - M5"));
+            add("name", random("Monitor DELL", "Iphone 15 pro", "Macbook air - m5"));
             add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
             add("price", random(Double.class, range(200, 459087.89)));
         }});
         Fixture.of(Product.class).addTemplate("valid", new Rule(){{
             add("id", random(Long.class, range(1L, 5L)));
-            add("name", random("Monitor", "iPhone 15", "Mac Book Pro", "Computador HP", "AirPods 11s"));
+            add("name", random("Monitor", "Iphone 15", "Macbook pro", "Computador", "Airpods 11s"));
             add("description", "Monitor da marca DELL de 30 polegadas. Possui uma tela touch screen, que permite usar tocando na tela.");
             add("price", random(Double.class, range(0.01, 199087.89)));
             add("isAvailable", true);
